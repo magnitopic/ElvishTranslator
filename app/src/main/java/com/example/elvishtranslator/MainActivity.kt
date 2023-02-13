86c819635591f1f969c4b06fccb4428e8fa3c3a2
magnitopic <magnitopic@gmail.com>
2023-02-13 11:31:33 +0100

    initial setup

--- app/src/main/java/com/example/elvishtranslator/MainActivity.kt
+++ app/src/main/java/com/example/elvishtranslator/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.elvishtranslator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeView(vista: View){
+        if (vista.id == R.id.textView){
+            var i = Intent(this, numeros::class.java)
+            startActivity(i)
+        }else if (vista.id == R.id.textView2){
+            var i =  Intent(this, familia::class.java)
+            startActivity(i)
+        }else if (vista.id == R.id.textView3){
+            var i =  Intent(this, colores::class.java)
+            startActivity(i)
+        }else if (vista.id == R.id.textView4){
+            var i =  Intent(this, numeros::class.java)
+            startActivity(i)
+        }
     }
 }
