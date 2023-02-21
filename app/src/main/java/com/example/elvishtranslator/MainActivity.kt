45a8b7f2794b83821b4e6003da2309507c45a37d
magnitopic <magnitopic@gmail.com>
2023-02-21 14:04:29 +0100

    new way of handdling button press

--- app/src/main/java/com/example/elvishtranslator/MainActivity.kt
+++ app/src/main/java/com/example/elvishtranslator/MainActivity.kt
@@ -4,25 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupListeners()
     }
 
-    fun changeView(vista: View){
-        if (vista.id == R.id.textView){
+    fun setupListeners() {
+        findViewById<TextView>(R.id.textView).setOnClickListener {
             var i = Intent(this, numeros::class.java)
             startActivity(i)
-        }else if (vista.id == R.id.textView2){
-            var i =  Intent(this, familia::class.java)
+        }
+        findViewById<TextView>(R.id.textView2).setOnClickListener {
+            var i = Intent(this, familia::class.java)
             startActivity(i)
-        }else if (vista.id == R.id.textView3){
-            var i =  Intent(this, colores::class.java)
+        }
+        findViewById<TextView>(R.id.textView3).setOnClickListener {
+            var i = Intent(this, numeros::class.java)
             startActivity(i)
-        }else if (vista.id == R.id.textView4){
-            var i =  Intent(this, numeros::class.java)
+        }
+        findViewById<TextView>(R.id.textView4){
+
+        }
+    }
+
+    fun changeView(vista: View) {
+        if (vista.id == R.id.textView) {
+        } else if (vista.id == R.id.textView2) {
+            var i = Intent(this, familia::class.java)
+            startActivity(i)
+        } else if (vista.id == R.id.textView3) {
+            var i = Intent(this, colores::class.java)
+            startActivity(i)
+        } else if (vista.id == R.id.textView4) {
+            var i = Intent(this, numeros::class.java)
             startActivity(i)
         }
     }
