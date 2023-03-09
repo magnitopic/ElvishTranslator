fab009a22ff4cec283990e491b1d352911450240
magnitopic <magnitopic@gmail.com>
2023-03-09 13:33:14 +0100

    new word object

--- app/src/main/java/com/example/elvishtranslator/Colores.kt
+++ app/src/main/java/com/example/elvishtranslator/Colores.kt
@@ -11,18 +11,18 @@ class Colores : AppCompatActivity() {
         setContentView(R.layout.activity_colores)
         val listaColores = findViewById<ListView>(R.id.lista_colores)
 
-        var colores= arrayListOf<String>(
-            "Rojo",
-            "Azul",
-            "Verede",
-            "Marrón",
-            "Fuxia",
-            "Morado",
-            "Granate",
-            "Amarillo",
-            "Rosa",
-            "Beige"
+        var colores = arrayListOf<Word>(
+            Word("Rojo", "Turpis"),
+            Word("Azul","Turpis"),
+            Word("Verede","Turpis"),
+            Word("Marrón","Turpis"),
+            Word("Fuxia","Turpis"),
+            Word("Morado","Turpis"),
+            Word("Granate","Turpis"),
+            Word("Amarillo","Turpis"),
+            Word("Rosa","Turpis"),
+            Word("Beige","Turpis")
         )
-        listaColores.adapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, colores)
+        listaColores.adapter = ArrayAdapter<Word>(applicationContext, android.R.layout.simple_list_item_1, colores)
     }
 }
---
+++ app/src/main/java/com/example/elvishtranslator/Word.kt
@@ -0,0 +1,3 @@
+package com.example.elvishtranslator
+
+data class Word(val spanish: String, val elvish: String)
